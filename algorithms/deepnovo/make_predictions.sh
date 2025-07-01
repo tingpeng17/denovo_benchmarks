0811f601e417818e767a4697eb6ee3f41edc1277
PominovaMS <marinapoms@gmail.com>
2025-07-01 13:37:58 +0200

    fixx deepnovo make_predictions script

--- algorithms/deepnovo/make_predictions.sh
+++ algorithms/deepnovo/make_predictions.sh
@@ -16,6 +16,9 @@ done <<< "$DSET_TAGS"
 # Iterate through files in the dataset
 for input_file in "$@"/*.mgf; do
 
+    # Clean previous input files
+    rm -rf ./input_data.mgf
+
     echo "Processing file: $input_file"
 
     # Convert input data to model format
